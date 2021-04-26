9697ede62dbfc63dbf7af40dd5be2dfdcae6319a
lingxi1322 <78313364+lingxi1322@users.noreply.github.com>
2021-04-26 15:48:45 +0800

    Update WPS_accept_invitation.py

--- WPS_accept_invitation.py
+++ WPS_accept_invitation.py
@@ -5,28 +5,28 @@ import requests
 
 
 sids = [
-    "V02StVuaNcoKrZ3BuvJQ1FcFS_xnG2k00af250d4002664c02f",
-    "V02SWIvKWYijG6Rggo4m0xvDKj1m7ew00a8e26d3002508b828",
-    "V02Sr3nJ9IicoHWfeyQLiXgvrRpje6E00a240b890023270f97",
-    "V02SBsNOf4sJZNFo4jOHdgHg7-2Tn1s00a338776000b669579",
-    "V02S2oI49T-Jp0_zJKZ5U38dIUSIl8Q00aa679530026780e96",
-    "V02ShotJqqiWyubCX0VWTlcbgcHqtSQ00a45564e002678124c",
+    "V02Sw9-goMQAxyEQu_EULMetb-INZo000af1142c00479959d8",
+    "V02S0R96lUd8ZEzQtyjxZX-kHQ2_tpk00a28f1650047c345ee",
+    "V02SBxbHOhf4rcYlBaXzTfJ8WC3B6PE00aa5f34d0038de5bac",
+    "V02SRvwTFf68enT4JrOPhQkKrKKqv5k00ab38f8500380673c8",
+    "V02SS1Aj322-C9Z-P36iZF2epL78zOQ00a7ea26400300425d3",
+    "V02S96aSosktKJiRKwEwBhqiWKHhxkE00a01300600374985fe",
     "V02SFiqdXRGnH5oAV2FmDDulZyGDL3M00a61660c0026781be1",
-    "V02S7tldy5ltYcikCzJ8PJQDSy_ElEs00a327c3c0026782526",
-    "V02SPoOluAnWda0dTBYTXpdetS97tyI00a16135e002684bb5c",
-    "V02Sb8gxW2inr6IDYrdHK_ywJnayd6s00ab7472b0026849b17",
-    "V02SwV15KQ_8n6brU98_2kLnnFUDUOw00adf3fda0026934a7f",
+    "V02SzpPttrDh9DtVZZjuSixmKOYsAuU00a98ee4a0047756591",
+    "V02SkTszoOYP88gtXvH1cXoDrMGoEGg00af1f571004626b252",
+    "V02SEdbqgnwva8UpfFOiw5BmksbY_JY00a53a2c300461f24f7",
+    "V02S6UQdQhot5IO9pmvBnEuiiVGcCYU00a6bc19c0047b369fa",
 #     "V02SC1mOHS0RiUBxeoA8NTliH2h2NGc00a803c35002693584d"
 ]
 mk = 0
 
-def request_re(sid, invite_userid, rep = 30):
+def request_re(sid, invite_userid, rep = 31):
     invite_url = 'http://zt.wps.cn/2018/clock_in/api/invite'
     r = requests.post( invite_url, headers={ 'sid': sid }, data={ 'invite_userid': invite_userid, "client_code": "040ce6c23213494c8de9653e0074YX30", "client": "alipay" } )
     js = json.loads(r.content)
     if js['msg'] == 'tryLater' and rep > 0:
         rep -= 1
-        time.sleep(10)
+        time.sleep(20)
         r = request_re(sid, invite_userid, rep)
     return r
 
